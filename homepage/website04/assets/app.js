ace942e221ea52563690f251bfaafdd9f1eab437
yuenu <yuenuni@hotmail.com>
2021-08-20 14:11:48 +0800

    feat: modify website04 done

--- homepage/website04/assets/app.js
+++ homepage/website04/assets/app.js
@@ -1,5 +1,14 @@
-let bookmark = document.querySelector("#bookmark");
+// Variables
+const bookmark = document.querySelector("#bookmark");
+const radioList = document.querySelectorAll('input[type="radio"]')
+const hamburgerMenu = document.querySelector('.hamburger')
 
+
+hamburgerMenu.addEventListener('click', function () {
+  hamburgerMenu.classList.toggle('active')
+})
+
+// Event Listener
 bookmark.addEventListener("click", function () {
   if (bookmark.lastElementChild.innerHTML === "bookmarked") {
     bookmark.lastElementChild.innerHTML = "bookmark";
@@ -8,3 +17,17 @@ bookmark.addEventListener("click", function () {
   }
   bookmark.classList.toggle("booked");
 });
+
+
+radioList.forEach((elem) => {
+  elem.addEventListener('change', function(e) {
+    // Remove all the 'active className'
+    for(let item of radioList) {
+      item.parentElement.parentElement.parentElement.parentElement.classList.remove('active')
+    }
+    // add the toggel One class
+    const card = e.target.parentElement.parentElement.parentElement.parentElement
+    card.classList.add('active')
+  })
+})
+
