75d724034f9c48b1a4d3d3bc54ec10d5d4f29e3b
yuenu <yuenuni@hotmail.com>
2021-09-23 22:41:53 +0800

    fix: mobile's header layout

--- homepage/website03/app.js
+++ homepage/website03/app.js
@@ -1,6 +1,5 @@
 let themeControl = document.getElementById('theme-control')
 let themeStyle = document.getElementById('theme-style')
-themeStyle.href="./assets/main.css"
 themeControl.addEventListener('change', function() {
   if(this.checked) {
     themeStyle.href="./assets/default.css"
