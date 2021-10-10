24d9786d730e187914c9964c6c82998b96929934
yuenu <yuenuni@hotmail.com>
2021-10-10 23:53:03 +0800

    feat: complete chat app basic feature

--- frontendmentor/component/chat-app-css-illustration-master/app.js
+++ frontendmentor/component/chat-app-css-illustration-master/app.js
@@ -1,24 +1,47 @@
+// Variable
 const returnBtn = document.querySelector("span.illustration__back");
 const moreBtn = document.querySelector("span.illustration__more");
-const moreList = document.querySelector('.more')
+const moreList = document.querySelector(".more");
 const userInput = document.querySelector(".illustration__footer input");
 const submitTextBtn = document.querySelector(".illustration__footer button");
 const textBody = document.querySelector(".illustration__body");
+const box = document.querySelector(".illustration__box");
+const contactList = document.querySelectorAll(
+  ".illustration__contact-message li"
+);
 
-returnBtn.addEventListener("click", () => {
-  console.log("return");
+// Return button event handle
+returnBtn.addEventListener("click", function () {
+  box.style.transform = "translateX(0)";
 });
 
-moreBtn.addEventListener("click", () => {
-  moreList.classList.toggle('active')
+// More button event handle
+moreBtn.addEventListener("click", function (e) {
+  if (moreList && !moreList.contains(e.target)) {
+    moreList.classList.toggle("active");
+  }
 });
 
+window.addEventListener("click", function (e) {
+  if (!moreBtn.contains(e.target)) {
+    moreList.classList.remove("active");
+  }
+});
+
+// User input submit handle
 userInput.addEventListener("change", (e) => {
-  const element = document.createElement('div')
-  element.classList.add('row')
-  element.classList.add('sent')
-  element.textContent = e.target.value
-  textBody.appendChild(element)
-  e.target.value = ''
-  textBody.scrollTop = textBody.scrollHeight
+  const element = document.createElement("div");
+  element.classList.add("row");
+  element.classList.add("sent");
+  element.textContent = e.target.value;
+  textBody.appendChild(element);
+  e.target.value = "";
+  textBody.scrollTop = textBody.scrollHeight;
+});
+
+// Contact list click event handle
+contactList.forEach((contacter) => {
+  contacter.addEventListener("click", function (e) {
+    box.style.transform = "translateX(-21.4rem)";
+  });
 });
