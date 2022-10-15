fa47d080a82e8427f78d228deb2ace583d0f4af7
yuenu <miraiku82@gmail.com>
2022-10-15 11:47:56 +0800

    feat(interactive-card): ui part done

---
+++ frontendmentor/interactive-card-details-form-main/app.js
@@ -0,0 +1,8 @@
+// Variable
+const ccnumInputEl = document.querySelector("#ccnum");
+const ccnum = document.querySelector(".card-front-line1");
+
+// Event Listener
+ccnumInputEl.addEventListener("input", (e) => {
+  console.log("ccumEL", e);
+});
